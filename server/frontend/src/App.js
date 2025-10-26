b0384c084eba0bacde662a3ef862842028a4cea5
fathima <fathimafidampz@gmail.com>
2025-10-26 03:35:27 -0400

    module3 updated lab3

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
 
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
      <Route path="/register" element={<Register />}/>
+     <Route path="/dealers" element={<Dealers/>} />
+     <Route path="/dealer/:id" element={<Dealer />} />
     </Routes>
   );
 }
